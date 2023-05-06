db9ec332d096b045f5fe1b7885daadc9d5add5bb
YangnyumChicken <goodngood1@gmail.com>
2023-05-06 14:34:01 +0900

    23.05.06
    들여쓰기 수정

--- phantom/HomePage/app/src/public/js/home/Cookie.js
+++ phantom/HomePage/app/src/public/js/home/Cookie.js
@@ -6,7 +6,7 @@ res.setHeader('Set-Cookie', 'Answer=5')
 
 if (!req.headers.cookie || req.headers.cookie.indexOf('Answer=') === -1) 
 {
-    res.setHeader('Set-Cookie', 'Answer=5');
+res.setHeader('Set-Cookie', 'Answer=5');
 } // Answer쿠키의 초기값은 5
 
 const cookies = req.headers.cookie;
@@ -29,26 +29,26 @@ let Answer = 1;
 
 for (let i = 0; i < cookies.length; i++) 
 {
-  const cookie = cookies[i].trim();
-  if (cookie.startsWith('Answer=')) 
-  {
+    const cookie = cookies[i].trim();
+if (cookie.startsWith('Answer=')) 
+{
     Answer = parseFloat(cookie.substring('Answer='.length), 10);
     break;
-  }
+}
 }
 
 if (Answer > 3 && Answer < 4) 
 {
-  alert('잘했어');
-  score(1);
+    alert('잘했어');
+    flag();
 } 
 else if (Answer >= 1 && Answer <= 3) 
 {
-  alert('조금 더 노력해봐');
+    alert('조금 더 노력해봐');
 } 
 else 
 {
-  alert('바보');
+    alert('바보');
 }
 const expires = new Date();
 expires.setDate(expires.getDate() + 1);
